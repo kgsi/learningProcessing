08c3817a9f66968035789864c876707ab215c4bf
kuusai <kuusai1998@gmail.com>
2017-08-22 09:17:16 +0900

    Icsmedia-particle>更新

--- particle/icsmedia-particle/sketch.js
+++ particle/icsmedia-particle/sketch.js
@@ -1,15 +1,14 @@
 
-
 var canvasWidth = 720;
 var canvasHeight = 480;
-var particleList = [];
 var particleSize = 10;
 var vx = 0;
 var vy = 0;
-
+var canvas = '';
 
 function setup() {
-  createCanvas(canvasWidth, canvasHeight);
+  canvas = createCanvas(canvasWidth, canvasHeight);
+  canvas.parent('wrap');
     
   noStroke();
   fill(0);
@@ -17,4 +16,19 @@ function setup() {
 }
 
 function draw() {
+  var particleList = [{x:0,y:0}];
+  clear();
+  // background('rgba(255,255,255, 0.1)');
+  
+  for (var i = 0; i < 20; i++) {
+    var obj = {x: canvasWidth * Math.random() ,y: canvasWidth * Math.random()};
+    
+    particleList.push(obj)
+    ellipse(particleList[i].x, particleList[i].y, particleSize, particleSize);
+    
+    // 配列に保存
+    particleList[i].x = canvasWidth + Math.random();
+    particleList[i].y = canvasWidth + Math.random();
+    console.log(particleList[i].x)
+  } 
 }
